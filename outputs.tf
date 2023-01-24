2be0d74ec581b87ca54c250abba9104fa381e7a0
sschimper-splunk <sschimper@splunk.com>
2023-01-24 10:38:26 +0100

    fixed small password bug

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,5 @@
-/*
-output "_1_url" {
-  description = "Public IP address of the EC2 instance hosted in the public subnet of the VPC."
-  value       = "http://${aws_eip.eip.public_ip}:8000/"
-}
-
-output "_2_username" {
-  description = "Username to log in"
-  value       = "admin"
-}
-
-output "_3_password" {
-  description = "Password to log in"
-  value       = "SPLUNK-${module.ec2-instance.id}"
-}
-
-
-output "splunk_instances" {
-  value = "URL: http://${module.ec2-instance.*.public_ip}:8000/, Username: admin, Password: ${module.ec2-instance.*.id}"
-}
-*/
-
 output "splunk_instances" {
   value = [
-    for instance in module.ec2-instance : "URL: http://${instance.public_ip}:8000/, Username: admin, Password: ${instance.id}"
+    for instance in module.ec2-instance : "URL: http://${instance.public_ip}:8000/, Username: admin, Password: SPLUNK-${instance.id}"
   ]
 }
